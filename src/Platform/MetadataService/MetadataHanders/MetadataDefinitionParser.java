4d34757ed32052e051f1993f79a9a578090abe4a
config_1 <subrata.besus@gmail.com>
2015-04-15 21:01:52 -0700

    Test Commit

--- src/Platform/MetadataService/MetadataHanders/MetadataDefinitionParser.java
+++ src/Platform/MetadataService/MetadataHanders/MetadataDefinitionParser.java
@@ -4,5 +4,11 @@ package Platform.MetadataService.MetadataHanders;
  * Created by subratabiswas on 4/15/15.
  */
 public class MetadataDefinitionParser {
-
+    private String[] _sqlCommands = new String[10];
+    public void initParsing()
+    {
+        //Read all folders from MetadataDefinition Folder
+        //Generate Sql
+        //Execute Sql
+    }
 }
